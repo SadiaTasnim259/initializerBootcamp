4998d9573c1b3879c28bef00838dbe775586bb44
Sadia Tasnim <tasnimsadia259@gmail.com>
2024-03-26 12:55:02 +0600

    Initial Commit

---
+++ initializerBootcamp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  initializerBootcamp
+//
+//  Created by Sadia on 26/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ initializerBootcamp/initializerBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  initializerBootcampApp.swift
+//  initializerBootcamp
+//
+//  Created by Sadia on 26/3/24.
+//
+
+import SwiftUI
+
+@main
+struct initializerBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
